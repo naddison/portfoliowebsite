0c4a4921f6f5379c5d71cb50eb964b4e3973247f
gaming <gaming@gaming>
2013-11-01 18:05:21 -0400

    "Finished mobile changes"

--- scripts/ImageSlider.js
+++ scripts/ImageSlider.js
@@ -11,18 +11,20 @@ $(document).ready(function() {
 	slidepaths[0] = 'images/githublink.jpg';
 	slidepaths[1] = 'images/hovercarslide.jpg';
 	slidepaths[2] = 'images/gluonslide.jpg';
+	slidepaths[3] = 'images/projservslide.jpg';
 	
 	//URL paths for each slide to go to
 	var slideDest = new Array();
 	slideDest[0] = 'https://github.com/naddison';
 	slideDest[1] = 'http://hovercardigital.com/';
 	slideDest[2] = 'http://99.239.85.142/gluon/controller/index.php';
+	slideDest[3] = 'projects.html';
 	
 	var position = 1;
 
         //if the next button is pressed
         $( '#next' ).on( 'click', function( event ) {
-			if (position < 2) {
+			if (position < 3) {
 				position = position + 1;
 			}
 				else {
@@ -36,7 +38,7 @@ $(document).ready(function() {
 	//if the previous button is pressed
         $( '#prev' ).on( 'click', function( event ) {
 			if (position <= 0) {
-				position = 2;
+				position = 3;
 			}
 			else {
 				position = position - 1;
